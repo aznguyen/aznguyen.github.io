6989da7ff3f53e3bd4be684e65119f6357f7b527
aznguyen <ngtdnghia@gmail.com>
2017-04-22 22:29:10 +0700

    1

--- js/customized.js
+++ js/customized.js
@@ -1,6 +1,6 @@
 var functionReady = () => {
     var out = "";
-   $.getJSON( "./Menus.json", function( data ) {
+   $.getJSON( "https://www.dropbox.com/s/p7ovxa1xmtvgwb2/Menus.json?dl=0", function( data ) {
        console.log(data);
     var items = [];
     $.each( data, function( key, val ) {
